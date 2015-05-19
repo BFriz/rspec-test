ca5a0d9f6cf22c93bfabf006c83befc5633374cc
Ben Frisby <bfrisb@gmail.com>
2015-05-19 23:25:51 +0100

    11 questions left with a couple wrong - haven't tried bonus

--- questions.rb
+++ questions.rb
@@ -68,8 +68,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-   seperated = array.group_by {|x| x % 2}
-    [seperated[0], seperated[1]]
+   seperated = array.group_by {|x| x % 2} 
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -77,40 +76,40 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
-  
+ array.find_all { |pal| pal == pal.reverse }.length
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-  
+  array.sort_by{|s| s.length }[0]
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
-  
+  array.sort_by{ |l| l.length }[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
-  
+  array.reduce(:+) 
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
-  
+  array + array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
-  
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
-  
+  array.inject(1) { |sum, x| sum + x } / array.size
 end
 
 # get all the elements in an array, up until the first element
@@ -118,14 +117,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
-  
+  array.select { |less| less < 6 }.uniq
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-  
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -133,14 +132,14 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  
+  array.map { |letter| letter.each_char.sort }.flatten.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
-  
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
@@ -210,7 +209,7 @@ end
 
 # get the square root of a number
 def square_root_of(number)
-  
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
